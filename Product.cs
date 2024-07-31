16b784fc633f2e9b74e066e1df09da2758d6edd4
Brett Smith <brett03112@gmail.com>
2024-07-31 17:44:16 -0400

    first commit completed exercise

---
+++ Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore
+{
+    internal class Product
+    {
+        protected static readonly List<Product> _products;
+
+        public decimal Price { get; set; }
+
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Description { get; set; }
+    }
+}
